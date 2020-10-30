37aa1127981b659f47a28fcc05370dde12d9499a
Ahmed Ali <hjaberty@gmail.com>
2020-10-29 22:24:37 -0400

    inital commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI":"mongodb+srv://ahmed:x44J7KaYs4JM3X7O@mongodbserver.1cgat.mongodb.net/favorite_books?retryWrites=true&w=majority"
+  
+};
